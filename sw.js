6ead31d00cb92a8beaa2774c5040de59af5b69c6
DEmanderbag <dusan-milosevic@outlook.com>
2022-04-09 23:14:27 +0200

    Update app to the latest version

--- sw.js
+++ sw.js
@@ -1,38 +1,38 @@
-const staticCacheName = 'white-label-v1';
+const staticCacheName = "white-label-v2";
 const assets = [
-  '/',
-  '/index.html',
-  '/app.js',
-  '/data/addons-en.json',
-  '/data/addons-sr.json',
-  '/data/burgers-en.json',
-  '/data/burgers-sr.json',
-  '/data/dips-en.json',
-  '/data/dips-sr.json',
-  '/data/sides-en.json',
-  '/data/sides-sr.json',
-  '/data/snacks-en.json',
-  '/data/snacks-sr.json',
-  '/data/sweets-en.json',
-  '/data/sweets-sr.json',
-  '/js/localization.js',
-  '/js/dates.js',
-  '/js/request.js',
-  '/scss/main.css',
-  '/assets/icons/share.svg',
-  '/assets/icons/close.svg',
-  '/assets/icons/cake-slice.svg',
-  '/assets/icons/corn-dog.svg',
-  '/assets/icons/donut.svg',
-  '/assets/icons/fried-egg.svg',
-  '/assets/icons/fried-potatoes.svg',
-  '/assets/icons/hamburger.svg',
-  '/assets/icons/seasoning.svg',
-  '/assets/icons/sandwich.svg',
-  '/assets/logos/favicon-32x32.png',
-  '/assets/logos/android-chrome-192x192.png',
-  '/manifest.json'
-]
+  "/",
+  "/index.html",
+  "/app.js",
+  "/data/addons-en.json",
+  "/data/addons-sr.json",
+  "/data/burgers-en.json",
+  "/data/burgers-sr.json",
+  "/data/dips-en.json",
+  "/data/dips-sr.json",
+  "/data/sides-en.json",
+  "/data/sides-sr.json",
+  "/data/snacks-en.json",
+  "/data/snacks-sr.json",
+  "/data/sweets-en.json",
+  "/data/sweets-sr.json",
+  "/js/localization.js",
+  "/js/dates.js",
+  "/js/request.js",
+  "/scss/main.css",
+  "/assets/icons/share.svg",
+  "/assets/icons/close.svg",
+  "/assets/icons/cake-slice.svg",
+  "/assets/icons/corn-dog.svg",
+  "/assets/icons/donut.svg",
+  "/assets/icons/fried-egg.svg",
+  "/assets/icons/fried-potatoes.svg",
+  "/assets/icons/hamburger.svg",
+  "/assets/icons/seasoning.svg",
+  "/assets/icons/sandwich.svg",
+  "/assets/logos/favicon-32x32.png",
+  "/assets/logos/android-chrome-192x192.png",
+  "/manifest.json",
+];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
@@ -46,7 +46,9 @@ self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) => {
       return Promise.all(
-        keys.filter((key) => key !== staticCacheName).map((key) => caches.delete(key))
+        keys
+          .filter((key) => key !== staticCacheName)
+          .map((key) => caches.delete(key))
       );
     })
   );
